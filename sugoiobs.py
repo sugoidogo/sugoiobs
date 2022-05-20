a9f37d4cd405b6a248c8186d3ffd6bacfb4e7257
josephsmendoza <josephsmendoza@gmail.com>
2022-05-19 18:36:46 -0600

    server re-write

--- sugoiobs.py
+++ sugoiobs.py
@@ -1,5 +1,5 @@
 from threading import Thread
-from os.path import basename,splitext,join
+from os.path import basename,splitext,join,normpath
 from sys import platform,path,executable
 from os import environ,makedirs,system
 from http.server import HTTPServer,SimpleHTTPRequestHandler
@@ -55,120 +55,76 @@ def pip_install(*args,target=join(get_data_dir(),'packages')):
     run([python,'-m','pip','install','--no-warn-script-location','--progress-bar=off','--target='+target,*args],shell=True)
 
 def start_server(static_dir=join(get_data_dir(),'static')):
-    class PluginHTTPRequestHandler(SimpleHTTPRequestHandler):
+    pathFunctions={
+        'GET':{},
+        'HEAD':{},
+        'POST':{},
+        'PUT':{},
+        'DELETE':{}
+    } # dict[method][pathRegex]=function
+    class RegexRequestHandler(SimpleHTTPRequestHandler):
         protocol_version='HTTP/1.1'
-        sse={} # Dict[str,List[BytesIO]]
-        sseListeners=[] # List[BytesIO]
-        def translate_path(self, path):
-            return join(static_dir,path[1:])
+        def translate_path(self,path):
+            path=normpath(join(static_dir,path))
+            if path.startswith(static_dir):
+                return path
+            self.send_error(403)
+            return static_dir
         def end_headers(self):
-            self.send_header('Access-Control-Allow-Origin', '*')
-            return super().end_headers()
-        def do_ERROR(self,*errors):
-            if(len(e)==1):
-                self.send_error(500,str(errors[0]),format_exc())
-                return print_exc()
-            message='multiple exceptions'
-            explain='multiple exceptions:\n'
-            for error in errors:
-                explain+=str(error)+'\n'
-                pass
-            self.send_error(500,message,explain)
-        def do_PUT_SSE(self):
-            if(self.path not in self.sse.keys()):
-                self.send_error(404)
-            client_count=0
-            for client in self.sse[self.path]:
-                if(client.closed):
-                    continue
-                client.write('data: '+self.rfile.read().decode()+'\n\n')
-                client.flush()
-                client_count+=1
-            if(client_count==0):
-                return self.send_error(404)
-            return self.send_response(200)
-        def do_PUT(self):
-            try:
-                if(self.path.endswith("#sse")):
-                    return self.do_PUT_SSE()
-                else:
-                    file_path=translate_path(self.path)
-                    file=open(file_path,'wb')
-                    file.write(self.rfile)
-                    file.close()
-                    return self.send_response(200)
-            except Exception as e:
-                return self.do_ERROR(e)
-        def do_GET_SSE_CLIENTS():
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/event-stream')
-            self.end_headers()
-            for path in self.sse.keys():
-                if(self.wfile.closed):
-                    return
-                self.wfile.write('data: '+path+'\n\n')
-            self.sseListeners.append(self.wfile)
-        def do_INFORM_SSE_LISTENERS(path):
-            for listener in self.sseListeners:
-                if(listener.closed):
-                    continue
-                listener.write('data: '+path+'\n\n')
-        def do_GET_SSE(self):
-            if(self.path not in self.sse.keys()):
-                self.sse[self.path]=[]
-            self.sse[self.path].append(self.wfile)
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/event-stream')
-            self.end_headers()
-            Thread(target=self.do_INFORM_SSE_LISTENERS,args=[path]).start()
-        def do_GET_AUDIO(self):
-            import sounddevice,numpy
-            if self.path == '/audio/':
-                response=json.dumps(sounddevice.query_devices(kind='input'))
-                if response.startswith('{'):
-                    response="["+response+"]"
-                response+="\n"
-                self.send_response(200)
-                self.send_header('Content-Type','text/json')
-                self.send_header('Content-Length',str(len(response)))
-                self.end_headers()
-                self.wfile.write(response.encode())
-                return
-            device=urllib.parse.unquote(self.path[len('/audio/'):]) or None
-            try:
-                sounddevice.check_input_settings(device=device)
-                self.send_response(200)
-                self.send_header('Content-Type','text/event-stream')
-                self.end_headers()
-            except Exception as e:
-                self.send_error(500,explain=str(e))
-            def audioLoop():
-                def audioCallback(indata, frames, time, status):
-                    try:
-                        volume=numpy.linalg.norm(indata)*10
-                        message='data: '+str(volume)+'\n\n'
-                        self.wfile.write(message.encode())
-                        self.wfile.flush()
-                    except:
-                        print_exc()
-                with sounddevice.InputStream(device=device,callback=audioCallback):
-                    while not self.wfile.closed:
-                        sounddevice.sleep(1)
-            Thread(target=audioLoop,name='sugoiobs.py audioLoop').start()
+            self.send_header('Access-Control-Allow-Origin','*')
+            super().end_headers()
+        def do_DISPATCH(self):
+            from re import match
+            for path, function in pathFunctions[self.command].items():
+                if(match(path,self.path)):
+                    Thread(target=function,args=[self]).start()
+                    return True
+            return False
         def do_GET(self):
+            self.do_DISPATCH() or super().do_GET()
+        def do_HEAD(self):
+            self.do_DISPATCH() or super().do_HEAD()
+        def do_POST(self):
+            self.do_DISPATCH()
+        def do_PUT(self):
+            self.do_DISPATCH()
+        def do_DELETE(self):
+            self.do_DISPATCH()
+    def audio(request):
+        import sounddevice,numpy
+        if request.path == '/audio/':
+            response=json.dumps(sounddevice.query_devices(kind='input'))
+            if response.startswith('{'):
+                response="["+response+"]"
+            response+="\n"
+            request.send_response(200)
+            request.send_header('Content-Type','text/json')
+            request.send_header('Content-Length',str(len(response)))
+            request.end_headers()
+            request.wfile.write(response.encode())
+            return
+        device=urllib.parse.unquote(request.path[len('/audio/'):]) or None
+        try:
+            sounddevice.check_input_settings(device=device)
+            request.send_response(200)
+            request.send_header('Content-Type','text/event-stream')
+            request.end_headers()
+        except Exception as e:
+            request.send_error(500,explain=str(e))
+        def audioCallback(indata, frames, time, status):
             try:
-                if self.path.startswith('/audio'):
-                    return self.do_GET_AUDIO()
-                if self.path=='/#sse':
-                    return self.do_GET_SSE_CLIENTS()
-                if self.path.endswith("#sse"):
-                    return self.do_GET_SSE()
-                return super().do_GET()
-            except Exception as e:
-                return self.do_ERROR(e)
-    makedirs(static_dir,exist_ok=True)
+                volume=numpy.linalg.norm(indata)*10
+                message='data: '+str(volume)+'\n\n'
+                request.wfile.write(message.encode())
+                request.wfile.flush()
+            except:
+                print_exc()
+        with sounddevice.InputStream(device=device,callback=audioCallback):
+            while not request.wfile.closed:
+                sounddevice.sleep(16)
+    pathFunctions['GET']['/audio']=audio
     global server
-    server=HTTPServer(('localhost',5000), PluginHTTPRequestHandler)
+    server=HTTPServer(('localhost',5000), RegexRequestHandler)
     Thread(target=server.serve_forever,name='sugoiobs.py HTTPServer').start()
 
 def open_data_dir(*args):
